df4df819907ae16fff1e1d3165bc88db75d03787
elcio <elcioferreira398@gmail.com>
2025-08-18 22:47:49 +0000

    adicionando o Express, versel.jason e renomeando o src para api

---
+++ ex01-express/api/index.js
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import express from "express";
+
+console.log("ola, turma!!!")
+
+console.log(process.env.MY_SECRET);
+
+const app = express();
+
+app.listen(3000, () =>
+  console.log('Example app listening on port 3000!'),
+);
--- ex01-express/src/index.js
+++
@@ -1,5 +0,0 @@
-import "dotenv/config";
-
-
-console.log("ola, turma!!!")
-console.log(process.env.MY_SECRET);
